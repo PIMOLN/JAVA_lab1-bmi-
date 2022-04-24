9f3f64a173ff1778be19e01b4d12e820ac4dc8b2
PIMOLN <104314501+PIMOLN@users.noreply.github.com>
2022-04-24 23:46:13 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,26 +3,38 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter weight in pounds: ");
-    double weight = scanner.nextDouble();
-    System.out.print("Enter height in inches: ");
-    double height = scanner.nextDouble();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number between 0 and 1000: ");
+        int number = scanner.nextInt();
+        int number1;
+        int number2;
+        int sum;
+        if(number<100)
+        {
+            number1 = number/10;
+            number2 = number%10;
+            sum = number1+number2;
+            System.out.println("The sum of the digits is "+sum);
+        }
+        else if(number>100 && number<=999)
+        {
+            int num1,num2,num3,num4,sumnum1;
+            num1 = number/100;
+            num2 = number/10;
+            num3 = num2%10;
+            num4 = number%10;
+            sumnum1 = num1+num3+num4;
+            System.out.println("The sum of the digits is " + sumnum1);
+        }
+        else if(number == 1000)
+        {
+            System.out.println("The sum of the digits is "+"1");
+        }
 
-    double pond =  0.45359237;
-    double inch =  0.0254;
-    double meter;
-    double kilo;
-    double meter1;
 
-    kilo = weight*pond;
-    meter  = height*inch;
-    meter1 = meter*meter;
 
-    double bmi;
-    bmi = kilo/meter1;
-    System.out.println("bmi is "+bmi);
+
+
 
     }
-
 }
